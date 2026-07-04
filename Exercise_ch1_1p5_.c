b94d8ff5f227f991cdb596cc73ec10601a123105
maria0170 <mushastam@gmail.com>
2026-07-04 18:32:20 -0400

    MyHelloWorld.c

---
+++ Exercise_ch1_1p5_.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Hello! My name is : Mariya\n");
+    printf("I am a student in : Computer Science\n");
+    printf("My favourite colour is : Red\n");
+    printf("My favourite fruit is : Cherries\n");
+    printf("\n");
+    printf("I am going to learn C! :)");
+
+    return 0;
+
+}
