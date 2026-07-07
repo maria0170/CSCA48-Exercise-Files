f16eb6fd9a01a01db84d8f929b9124cc7a044fea
maria0170 <mushastam@gmail.com>
2026-07-07 13:30:38 -0400

    Add program to output alphabet letters A to Z

---
+++ Exercise_ch1_9_1p1_.c
@@ -0,0 +1,12 @@
+/* Write a program that outputs the letters in the alphabet (from A to Z)*/
+
+#include<stdio.h>
+
+int main(){
+    for (char i =65; i < 91; i++){
+        printf("%c\n", i);
+    }
+  
+    return 0;
+
+}
