901e9fb8fc0057e88f4567ef18e874a5898f8637
maria0170 <mushastam@gmail.com>
2026-07-08 10:05:44 -0400

    Add character pairing output in main function

--- Exercise_ch1_9_1p2_.c
+++ Exercise_ch1_9_1p2_.c
@@ -25,3 +25,23 @@ int main(){
     return 0;
 
 }
+
+
+// also can be done like this 
+#include <stdio.h>
+
+int main (){
+
+    char i = 65;
+    char j = 97;
+
+    while (i<=90 && j <= 122)
+    {
+        printf("%c - %c\n", i, j);
+        i = i+1;
+        j = j+1;
+    }
+
+
+    return 0;
+}
