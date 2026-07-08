9d1f12fbed902d9824d8ae795625ab833ec3cc68
maria0170 <mushastam@gmail.com>
2026-07-08 10:14:50 -0400

    Implement prime number checking and printing
    The is_prime function currently only checks if a number is 2 or even. It needs to be updated to check for other prime conditions.

--- Exercise_ch1_9_1p3_.c
+++ Exercise_ch1_9_1p3_.c
@@ -1,1 +1,29 @@
+/* Write a program that prints out the prime numbers between 2 and 100
+*/
 
+#include<stdio.h>
+// returns 1 if prime and 0 if not prime 
+int is_prime(int val){
+     if (val == 2){
+        return 1;
+    }
+    else if(val % 2 == 0){
+        return 0;
+    }
+
+    return 1; 
+    }
+
+
+int main(){
+    
+    for (int i = 2; i <=100; i++){
+        if(is_prime(i)== 1){
+            printf("%d\n", i);
+        }
+
+    }
+
+    return 0;
+
+}
