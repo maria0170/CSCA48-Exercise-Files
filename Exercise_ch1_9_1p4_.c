d6d4a74d2ea8e2af656f091997f3ecfde288b6f6
maria0170 <mushastam@gmail.com>
2026-07-08 11:35:06 -0400

    Add prime number printing program
    Implemented a program to print prime numbers between 2 and 100.

--- Exercise_ch1_9_1p4_.c
+++ Exercise_ch1_9_1p4_.c
@@ -1,1 +1,27 @@
-// to do 
+/* Write a program that prints out the prime numbers between 2 and 100
+*/
+#include<stdio.h>
+
+// return 1 if it is prime and 0 if it is not prime 
+int is_prime(int n){
+    if (n <=1){
+        return 0;
+    }
+    for (int i = 2; i < n; i++){
+        if(n % i == 0){
+            return 0; 
+        }
+    }
+    return 1;
+}
+int main(){
+    
+    for (int i =2; i<=100; i++){
+        if(is_prime(i)==1){
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+
+}
